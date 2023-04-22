24d18cd209a05a484477c8b27494f3ca21be49ae
Adarsh Ravishankar <89323486+AdarshRavis@users.noreply.github.com>
2023-04-22 17:53:54 -0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,20 @@
+# Set the app title
+st.title("Molecule Similarity Calculator")
+    
+# Get the SMILES notations from the user
+smiles1 = st.text_input("Enter the SMILES notation for molecule 1:")
+smiles2 = st.text_input("Enter the SMILES notation for molecule 2:")
+    
+# Generate RDKit molecules from the SMILES strings
+mol1 = Chem.MolFromSmiles(smiles1)
+mol2 = Chem.MolFromSmiles(smiles2)
+    
+# Calculate the ECFP fingerprints for each molecule
+fp1 = AllChem.GetMorganFingerprintAsBitVect(mol1, 2, nBits=2048)
+fp2 = AllChem.GetMorganFingerprintAsBitVect(mol2, 2, nBits=2048)
+    
+# Calculate the Tanimoto similarity index between the two fingerprints
+similarity = DataStructs.TanimotoSimilarity(fp1, fp2)
+    
+# Display the similarity index to the user
+st.write(f"The Tanimoto similarity index between the two molecules is: {similarity:.2f}")
